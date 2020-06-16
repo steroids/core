f43cede23e7aed836d8807784cace03bf7a229b6
Vladimir Kozhin <hello@kozhindev.com>
2020-06-16 14:22:42 +0700

    add providers trait

---
+++ traits/ModuleProvidersTrait.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace steroids\core\traits;
+
+use steroids\auth\providers\BaseAuthProvider;
+use steroids\notifier\providers\BaseNotifierProvider;
+use yii\helpers\ArrayHelper;
+
+trait ModuleProvidersTrait {
+
+    /**
+     * @var BaseAuthProvider[]|array
+     */
+    public array $providers;
+
+    //public array $providersClasses;
+
+    /**
+     * @param string $name
+     * @return BaseNotifierProvider|null
+     * @throws \yii\base\InvalidConfigException
+     */
+    public function getProvider($name)
+    {
+        if (!$this->providers || !isset($this->providers[$name])) {
+            return null;
+        }
+        if (is_array($this->providers[$name])) {
+            $this->providers[$name] = \Yii::createObject(array_merge(
+                ['className' => ArrayHelper::getValue($this->providersClasses, $name)],
+                $this->providers[$name],
+                ['name' => $name]
+            ));
+        }
+        return $this->providers[$name];
+    }
+
+}
