4968f7e82f21a81f140a9c9274598352d9a831ca
gosha <nitrogosha2014@mail.ru>
2020-07-02 22:01:37 +0700

    initialized provider property

--- traits/ModuleProvidersTrait.php
+++ traits/ModuleProvidersTrait.php
@@ -4,6 +4,8 @@ namespace steroids\core\traits;
 
 use steroids\auth\providers\BaseAuthProvider;
 use steroids\notifier\providers\BaseNotifierProvider;
+use Yii;
+use yii\base\InvalidConfigException;
 use yii\helpers\ArrayHelper;
 
 trait ModuleProvidersTrait {
@@ -11,14 +13,14 @@ trait ModuleProvidersTrait {
     /**
      * @var BaseAuthProvider[]|array
      */
-    public array $providers;
+    public array $providers = [];
 
     //public array $providersClasses;
 
     /**
      * @param string $name
      * @return BaseNotifierProvider|null
-     * @throws \yii\base\InvalidConfigException
+     * @throws InvalidConfigException
      */
     public function getProvider($name)
     {
@@ -26,7 +28,7 @@ trait ModuleProvidersTrait {
             return null;
         }
         if (is_array($this->providers[$name])) {
-            $this->providers[$name] = \Yii::createObject(array_merge(
+            $this->providers[$name] = Yii::createObject(array_merge(
                 ['class' => ArrayHelper::getValue($this->providersClasses, $name)],
                 $this->providers[$name],
                 ['name' => $name]
